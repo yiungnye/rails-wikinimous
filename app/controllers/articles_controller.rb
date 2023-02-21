dcde6d0133c5e7ebc230e8cf42834b8b28746fd1
Jeremy Chan Yue Nang <yiungnye@gmail.com>
2023-02-21 14:42:01 +0400

    finished create step

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,19 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.create(article_params)
+    redirect_to article_path(@article)
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
 
   get 'articles', to: 'articles#list'
 
+  get 'articles/new', to: 'articles#new'
   get 'articles/:id', to: 'articles#show', as: 'article'
+  post 'articles', to: 'articles#create', as: 'create_article'
 end
